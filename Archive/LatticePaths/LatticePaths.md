46867b9f82957d04d8379738b423aba24caaf2ca
OZcomingFRoo <OZcomingFRoo@gmail.com>
2019-04-30 18:49:34 +0300

    Added new Task Updated previous task

---
+++ Archive/LatticePaths/LatticePaths.md
@@ -0,0 +1,4 @@
+### -------------- Lattice Paths --------------
+Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+
+[Link to Task](https://projecteuler.net/problem=15 "Link to Task")
--- Archive/SmallestMultiple/SmallestMultiple.test.js
+++ Archive/SmallestMultiple/SmallestMultiple.test.js
@@ -5,7 +5,7 @@ test("2520 is the smallest number that can be divided by each of the numbers fro
   expect(findSmallestMultiplierForNumRange(1,10)).toEqual(2520);
 });
 
-// Task... 
+// 232792560 is the smallest number that can be divided by each of the numbers from 1 to 20 
 test("232792560 is the smallest number that can be divided by each of the numbers from 1 to 20", () => {
-  expect(findSmallestMultiplierForNumRange(1,20)).toBeDefined();
+  expect(findSmallestMultiplierForNumRange(1,20)).toEqual(232792560);
 });
